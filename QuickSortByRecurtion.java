7e50fde4202694c1cc1076e5a9b8cbba2e27e767
C1507930 <1990145701@qq.com>
2016-01-07 22:55:33 +0800

    我的递归快速排序

---
+++ QuickSortByRecurtion.java
@@ -0,0 +1,35 @@
+package test.quicksort;
+
+//递归快速排序实现
+public class QuickSortByRecurtion {
+
+	public static void quick_sort(int[] s, int l, int r) {
+		if (l < r) {
+			// Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+			int i = l, j = r, x = s[l];
+			while (i < j) {
+				while (i < j && s[j] >= x)
+					// 从右向左找第一个小于x的数
+					j--;
+				if (i < j)
+					s[i++] = s[j];
+
+				while (i < j && s[i] < x)
+					// 从左向右找第一个大于等于x的数
+					i++;
+				if (i < j)
+					s[j--] = s[i];
+			}
+			s[i] = x;
+			quick_sort(s, l, i - 1); // 递归调用
+			quick_sort(s, i + 1, r);
+		}
+	}
+	public static void main(String[] args) {
+		int[] arr = { 49, 38, 65, 97, 76, 13, 27 };
+		QuickSortByRecurtion.quick_sort(arr, 0, arr.length-1);
+		for(int i=0;i<arr.length;i++){
+			System.out.println(arr[i]);
+		}
+	}
+}
